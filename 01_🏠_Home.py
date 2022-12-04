64ecddb59e51b90335a5e71ca7545a90805bed47
fsmosca <fsmosca@users.noreply.github.com>
2022-12-04 12:09:21 +0800

    Update 01_🏠_Home.py

--- 01_🏠_Home.py
+++ 01_🏠_Home.py
@@ -1,9 +1,18 @@
 import streamlit as st
 from modules.config import Config
+from modules.constants import BOARD_DEFAULT_VALUE
 
 
 myconfig = Config()
 myconfig.set_config()
+
+
+# Updates the state variables on all modules.
+st.session_state.update(st.session_state)
+
+
+if 'board_size_k' not in st.session_state:
+    st.session_state.board_size_k = BOARD_DEFAULT_VALUE
 
 
 def main():
